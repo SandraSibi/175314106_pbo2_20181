9f0db403d9454b9ad4d8ee182c56854aa0946212
admin <admin@12b>
2018-08-29 12:12:03 +0700

    versi 0.1

---
+++ src/Praktek_1/Dokter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktek_1;
+
+/**
+ *
+ * @author admin
+ */
+public class Dokter {
+    private String nomorPegawai;
+    private String nama;
+    private String alamat;
+    private String temptLahir;
+    private int tanggalLahir;
+ 
+}
